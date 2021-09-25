d8e2d88c38fcf238fb4696a23dbfd51250b53125
Feng DING <dingbobby@hotmail.com>
2021-09-25 15:06:30 +0800

    <feat>(demo): add demo

---
+++ README.md
@@ -0,0 +1,14 @@
+# Description
+This project is a demo which show you how to use [apollo cyber](https://github.com/FengD/apollo_cyber).
+
+# Steps
+1. Create your own [proto](test.proto) file. You could also use the pre-defined [proto](https://github.com/FengD/apollo_cyber/tree/master/sensor_proto) in [cyber project](https://github.com/FengD/apollo_cyber);
+2. Read the user guide of apollo cyber.
+3. [develop tools](https://github.com/ApolloAuto/apollo/blob/master/docs/cyber/CyberRT_Developer_Tools.md)
+
+* cyber_monitor
+![1](images/1.png)
+![2](images/2.png)
+* cyber_recorder
+![3](images/3.png)
+![4](images/4.png)
--- talker.cc
+++ talker.cc
@@ -1,5 +1,5 @@
 #include "cyber/cyber.h"
-#include "sensor_proto/lidar.pb.h"
+#include "test.pb.h"
 
 using apollo::cyber::Rate;
 using apollo::cyber::Time;
@@ -8,15 +8,15 @@ int main(int argc, char *argv[]) {
   // init cyber framework
   apollo::cyber::Init(argv[0]);
   // create talker node
-  auto talker_node = apollo::cyber::CreateNode("talker");
+  auto talker_node = apollo::cyber::CreateNode("test");
   // create talker
-  auto talker = talker_node->CreateWriter<crdc::airi::PointCloud>("channel/chatter");
+  auto talker = talker_node->CreateWriter<test::Test>("test");
   Rate rate(100.0);
   uint64_t seq = 0;
   while (apollo::cyber::OK()) {
-    auto msg = std::make_shared<crdc::airi::PointCloud>();
-    msg->mutable_header()->set_lidar_timestamp(Time::Now().ToNanosecond());
-    msg->mutable_header()->set_sequence_num(seq);
+    auto msg = std::make_shared<test::Test>();
+    msg->set_a(Time::Now().ToNanosecond());
+    msg->set_b(seq);
     talker->Write(msg);
     AINFO << "talker sent a message! No. " << seq;
     seq++;
